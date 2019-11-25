3bcdb9f2bfee225062788f27d955008d70972dd5
Christian Martel <christian.martel2mail.mcgill.ca>
2019-11-25 13:18:47 -0500

    changed display so it displays

--- src/ca/mcgill/ecse211/project/Display.java
+++ src/ca/mcgill/ecse211/project/Display.java
@@ -29,8 +29,8 @@ public class Display implements Runnable {
       
       // Print x,y, and theta information
       DecimalFormat numberFormat = new DecimalFormat("######0.00");
-      GameResources.LCD.drawString("X: " + numberFormat.format(position[0]), 0, 0);
-      GameResources.LCD.drawString("Y: " + numberFormat.format(position[1]), 0, 1);
+      GameResources.LCD.drawString("X: " + numberFormat.format(position[0]/GameResources.TILE_SIZE), 0, 0);
+      GameResources.LCD.drawString("Y: " + numberFormat.format(position[1]/GameResources.TILE_SIZE), 0, 1);
       GameResources.LCD.drawString("T: " + numberFormat.format(position[2]), 0, 2);
       
       // this ensures that the data is updated only once every period
